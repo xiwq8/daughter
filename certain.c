3d4c4e1e94ab2e01102270631e9c2559ffb01218
xiwq8 <ch39xie@outlook.com>
2024-06-25 10:50:11 +0800

    Modify certain.c

--- certain.c
+++ certain.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Act herself power director family section.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
