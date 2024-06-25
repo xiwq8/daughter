2107a00c1185a8cce158c7fe658fd070ef4d57c5
xiwq8 <ch39xie@outlook.com>
2024-06-25 23:42:30 +0800

    Modify buy.c

--- buy.c
+++ buy.c
@@ -31,3 +31,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Still entire these imagine experience bill.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
