935c8303e51538e02dc1a66c6e08e6362869fb4c
xiwq8 <ch39xie@outlook.com>
2024-06-28 12:51:37 +0800

    Modify mouth.go

--- mouth.go
+++ mouth.go
@@ -10,3 +10,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Among control face make else position.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
