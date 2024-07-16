46cbae106f0b4dbc1b9004d6c62dc7756803999e
xiwq8 <ch39xie@outlook.com>
2024-07-16 10:33:28 +0800

    Modify buy.c

--- buy.c
+++ buy.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Policy provide student college attorney far.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
