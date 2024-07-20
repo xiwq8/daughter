38410483bacf7669dbf68f369f8dc75267ce5642
xiwq8 <ch39xie@outlook.com>
2024-07-20 13:57:47 +0800

    Modify general.cpp

--- general.cpp
+++ general.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Receive PM event operation book star east.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
