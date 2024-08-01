51c5c47f1336ee14683b704ba6d8ca09dbe6f20f
xiwq8 <ch39xie@outlook.com>
2024-08-01 12:36:49 +0800

    Modify trouble.cpp

--- trouble.cpp
+++ trouble.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Energy very father pass writer attorney knowledge.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
