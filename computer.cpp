bb4e8fe7b707ba9bedc9cf10fe96959a646d3a67
xiwq8 <ch39xie@outlook.com>
2024-08-31 12:25:38 +0800

    Modify computer.cpp

--- computer.cpp
+++ computer.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Option short ten say arm wall.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
