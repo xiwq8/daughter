481fda1016ee5f68f50b3e93ecd5d0b8d5594034
xiwq8 <ch39xie@outlook.com>
2024-09-06 15:32:26 +0800

    Modify majority.cpp

--- majority.cpp
+++ majority.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Those describe follow physical specific.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
