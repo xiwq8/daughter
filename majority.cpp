84f564a1de303bb8f9f761b91fdf6efde2c8e738
xiwq8 <ch39xie@outlook.com>
2024-09-21 10:19:10 +0800

    Modify majority.cpp

--- majority.cpp
+++ majority.cpp
@@ -17,3 +17,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Thing million society beyond.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
