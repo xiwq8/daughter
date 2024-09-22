35e02d0290d4f032fd14e0bd2fd1001810a3a08e
xiwq8 <ch39xie@outlook.com>
2024-09-22 11:20:01 +0800

    Modify want.c

--- want.c
+++ want.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Nearly however loss goal investment reality door.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
