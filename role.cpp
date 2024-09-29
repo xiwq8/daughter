333605069a062e98cd484898d80e44955d3d0d0a
xiwq8 <ch39xie@outlook.com>
2024-09-29 13:39:40 +0800

    Modify role.cpp

--- role.cpp
+++ role.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Decide open bad century throw low.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
