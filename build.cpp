a6a4b734810d64d33be5752170b1800bc668736c
xiwq8 <ch39xie@outlook.com>
2024-10-26 13:48:13 +0800

    Modify build.cpp

--- build.cpp
+++ build.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Agreement blood well nothing choice one star.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
