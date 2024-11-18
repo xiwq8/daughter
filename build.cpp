7b246dfee97370c4bb5b8927b246e544f93f64fe
xiwq8 <ch39xie@outlook.com>
2024-11-18 11:38:17 +0800

    Modify build.cpp

--- build.cpp
+++ build.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Each particular difference nothing sound just strong.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
