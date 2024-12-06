94e9036a9be1bddab4f12ecfb8b00f162ff8409b
xiwq8 <ch39xie@outlook.com>
2024-12-06 15:14:20 +0800

    Modify door.c

--- door.c
+++ door.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# True career final foreign toward idea pay tax.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
