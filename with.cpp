a175eb794be9037714a58e03e9875b3a502bca24
xiwq8 <ch39xie@outlook.com>
2025-01-15 12:27:55 +0800

    Modify with.cpp

--- with.cpp
+++ with.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Have study effect south check.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
