7ba275c0ed80ec5609c0304ea2587cc72af3796e
xiwq8 <ch39xie@outlook.com>
2025-02-07 23:45:24 +0800

    Modify finally.go

--- finally.go
+++ finally.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Religious surface stand old particular simply.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
