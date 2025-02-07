bb32b7f6ae21c3db6f4f793d14aa8b9d64a79923
xiwq8 <ch39xie@outlook.com>
2025-02-07 23:45:24 +0800

    Modify fill.cpp

--- fill.cpp
+++ fill.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Understand course stand employee mention.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
